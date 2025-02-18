8716449be4c048751716cb71300b092631d55354
PatriciaTot <116910162+PatriciaTot@users.noreply.github.com>
2025-02-19 00:30:09 +0100

    exercice 2

--- src/components/pictures.tsx
+++ src/components/pictures.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { picturesSelector } from '../reducer';
 
 const Container = styled.div`
   padding: 1rem;
@@ -17,8 +19,17 @@ const Image = styled.img`
     transform: scale(1.2);
   }
 `;
+
 const Pictures = () => {
-  return null;
+  const pictures = useSelector(picturesSelector);
+
+  return (
+    <Container>
+      {pictures.map((picture, index) => (
+        <Image key={index} src={picture.previewFormat} alt={picture.author} />
+      ))}
+    </Container>
+  );
 };
 
 export default Pictures;
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,22 +1,34 @@
 import { Loop, liftState } from 'redux-loop';
 import { compose } from 'redux';
 import { Actions } from './types/actions.type';
+import { Picture } from './types/picture.type';
+import fakeData from './fake-datas.json';
 
 export type State = {
   counter: number,
+  pictures: Picture[];
 }
 
 export const defaultState: State = {
-  counter: 0
+  counter: 0,
+  pictures: [],
 }
 
 export const reducer = (state: State | undefined, action: Actions): State | Loop<State> => {
   if (!state) return defaultState; // mandatory by redux
   switch (action.type) {
     case 'INCREMENT':
-      return { ...state, counter: state.counter + 1 };
+      return {
+        ...state,
+        counter: state.counter + 1,
+        pictures: fakeData.slice(0, state.counter + 1),
+      };
     case 'DECREMENT':
-      return state.counter > 3 ? { ...state, counter: state.counter - 1 } : state;
+      return state.counter > 3 ? {
+        ...state,
+        counter: state.counter - 1,
+        pictures: fakeData.slice(0, state.counter - 1),
+      } : state;
     case 'SELECT_PICTURE':
       throw 'Not Implemented';
     case 'CLOSE_MODAL':
@@ -34,9 +46,8 @@ export const reducer = (state: State | undefined, action: Actions): State | Loop
 
 export const counterSelector = (state: State) => state.counter;
 
-export const picturesSelector = (state: State) => {
-  throw 'Not Implemented';
-};
+export const picturesSelector = (state: State) => state.pictures;
+
 export const getSelectedPicture = (state: State) => {
   throw 'Not Implemented';
 };
